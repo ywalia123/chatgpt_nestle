50f8a9e0fbc77c31470f9bcb95eae0df3c8ef845
ywalia123 <139229831+ywalia123@users.noreply.github.com>
2023-07-18 18:29:16 +0530

    app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
- from gpt_index import SimpleDirectoryReader, GPTListIndex, GPTSimpleVectorIndex, LLMPredictor, PromptHelper
- from langchain import OpenAI
- import gradio as gr
- import sys
- import os
+from gpt_index import SimpleDirectoryReader, GPTListIndex, GPTSimpleVectorIndex, LLMPredictor, PromptHelper
+from langchain import OpenAI
+import gradio as gr
+import sys
+import os
 
 os.environ["OPENAI_API_KEY"] = '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'
 
