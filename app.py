2e184d2b26f00378e9a7f1808acffdd12518f013
ywalia123 <139229831+ywalia123@users.noreply.github.com>
2023-07-18 18:10:26 +0530

    app.py

--- app.py
+++ app.py
@@ -1,13 +1,8 @@
-# from gpt_index import SimpleDirectoryReader, GPTListIndex, GPTSimpleVectorIndex, LLMPredictor, PromptHelper
-# from langchain import OpenAI
-# import gradio as gr
-# import sys
-# import os
-from gpt_index import SimpleDirectoryReader, GPTListIndex, GPTSimpleVectorIndex, LLMPredictor, PromptHelper
-from langchain.chat_models import ChatOpenAI
-import gradio as gr
-import sys
-import os
+ from gpt_index import SimpleDirectoryReader, GPTListIndex, GPTSimpleVectorIndex, LLMPredictor, PromptHelper
+ from langchain import OpenAI
+ import gradio as gr
+ import sys
+ import os
 
 os.environ["OPENAI_API_KEY"] = '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'
 
